863ed9fd1901dd29345108788ac9e0e997b8061d
Aryan-Ashta <aashta221@gmail.com>
2026-07-17 10:16:09 -0400

    implemented the forward pass in the triton kernel

--- kernels.py
+++ kernels.py
@@ -1,3 +1,33 @@
 import triton
 import triton.language as tl
-# future kernels written here
+
+@triton.jit
+def rmsnorm_fw_kernel(
+    X_ptr, # ptr to input
+    Y_ptr, # ptr to output
+    W_ptr, # ptr to weight parameter Gamma
+    rstd_ptr, # ptr to output reciprocal std dev (backward pass)
+    stride_row, # distance between memory rows
+    N_cols, # cols per row
+    eps,
+    BLOCK_SIZE: tl.constexpr
+):
+    row_idx = tl.program_id(0) # map instance to row
+
+    row_start_ptr = X_ptr + row_idx * stride_row
+    col_offsets = tl.arange(0, BLOCK_SIZE)
+    mask = col_offsets < N_cols # computing memory offsets
+
+    x = tl.load(row_start_ptr + col_offsets, mask=mask, other=0.0).to(tl.float32) # load x into SRAM
+    w = tl.load(W_ptr + col_offsets, mask=mask, other=0.0).to(tl.float32) # load w into SRAM
+
+    x_sq = x*x
+    mean_sq = tl.sum(x_sq, axis=0) / N_cols
+    rstd = tl.math.rsqrt(mean_sq + eps) # computed RMS
+
+    tl.store (rstd_ptr + row_idx, rstd) # write reciprocal std dev for backward pass
+
+    y = x * rstd * w # normalize and scale
+
+    y_row_start_ptr = Y_ptr + row_idx * stride_row
+    tl.store(y_row_start_ptr + col_offsets, y, mask=mask) # store back to HBM
--- rmsnorm.py
+++ rmsnorm.py
@@ -1,5 +1,47 @@
 import torch
 import torch.nn as nn
+import triton
+from kernels import rmsnorm_fw_kernel
+
+class FusedRMSNormFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, weight, eps=1e-5):
+        orig_shape = x.shape
+        x_2d = x.view(-1, orig_shape[-1])
+        M, N = x_2d.shape #handle input tensors
+
+        y = torch.empty_like(x_2d) # output tensor allocation
+        rstd = torch.empty((M,),dtype=torch.float32, device=x.device) # allocate tensor to save rstd
+
+        BLOCK_SIZE = triton.next_power_of_2(N) #heuristic to set BLOCK_SIZE
+
+        grid = (M,)
+
+        rmsnorm_fw_kernel[grid](
+            x_2d, y, weight, rstd,
+            x_2d.stride(0), N, eps,
+            BLOCK_SIZE=BLOCK_SIZE
+        )
+
+        ctx.save_for_backward(x, weight, rstd)
+        ctx.eps = eps
+        return y.view(*orig_shape)
+
+        @staticmethod
+        def backward(ctx, dy):
+            raise NotImplementedError("backward pass is not implemented yet")
+
+class FusedRMSNorm(nn.Module):
+    # Fused RMS Norm
+    def __init__(self, dim: int, eps: float = 1e-5):
+        super().__init__()
+        self._norm = NaiveRMSNorm(dim, eps)
+
+        self.compiled_norm = torch.compile(self._norm)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return FusedRMSNormFunction.apply(x, self.weight, self.eps)
+
 
 # profiler and baseline for existing PyTorch RMSNorm
 
